0be2bfedba20e5a31a6eb067c0264da61b13add8
Emil <e.yakoff4@gmail.com>
2022-08-08 16:23:59 +0300

    modified v 1.1 with exchanging to RUB

--- js/script.js
+++ js/script.js
@@ -2,22 +2,43 @@ document.addEventListener('DOMContentLoaded', () => {
    const inputRub = document.querySelector('#rub'),
          inputUsd = document.querySelector('#usd');
 
-   inputRub.addEventListener('input', () => {
-      // console.log('Hie');
+   inputRub.addEventListener('input', (e) => {
+      const currencyRub = e.target.id;
+      requestForCurrency(currencyRub);
+   });
+
+   inputUsd.addEventListener('input', (e) => {
+      const currencyUsd = e.target.id;
+      requestForCurrency(currencyUsd);
+   });
+
+   function requestForCurrency(currency) {
       const request = new XMLHttpRequest();
 
       request.open('GET', 'js/currency.json');
       request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
       request.send();
 
-      request.addEventListener('load', () => {
-         if(request.status === 200) {
-            const data = JSON.parse(request.response);
-
-            inputUsd.value = (+inputRub.value / data.currency.usd).toFixed(2);
-         } else {
-            inputUsd.value = 'Что-то пошло не так';
-         }
-      });
-   });
+      if (currency === 'rub') {
+         request.addEventListener('load', () => {
+            if(request.status === 200) {
+               const data = JSON.parse(request.response);
+               inputUsd.value = (+inputRub.value / data.currency.usd).toFixed(2);
+            } else {
+               inputUsd.value = 'Что-то пошло не так';
+            }
+         });
+      } else if (currency === 'usd') {
+         request.addEventListener('load', () => {
+            if(request.status === 200) {
+               const data = JSON.parse(request.response);
+               inputRub.value = (+inputUsd.value * data.currency.usd).toFixed(2);
+            } else {
+               inputRub.value = 'Что-то пошло не так';
+            }
+         });
+      } else {
+         console.log('Error');
+      }
+   }
 });
